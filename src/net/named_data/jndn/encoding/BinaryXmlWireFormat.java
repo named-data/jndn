d8021a63c45fb13d5929de8b85ae52adde44b3d6
Jeff Thompson <jefft0@gmail.com>
2013-11-15 13:05:36 -0800

    Added encodeInterest and decodeInterest, minus Exclude.

--- src/net/named_data/jndn/encoding/BinaryXmlWireFormat.java
+++ src/net/named_data/jndn/encoding/BinaryXmlWireFormat.java
@@ -6,10 +6,88 @@
 
 package net.named_data.jndn.encoding;
 
+import java.nio.ByteBuffer;
 import net.named_data.jndn.Name;
 import net.named_data.jndn.util.Blob;
+import net.named_data.jndn.Interest;
 
 public class BinaryXmlWireFormat extends WireFormat {
+  /**
+   * Encode interest in binary XML and return the encoding.
+   * @param interest The Interest object to encode.
+   * @return A Blob containing the encoding.
+   */  
+  @Override
+  public Blob 
+  encodeInterest(Interest interest)
+  {
+    BinaryXmlEncoder encoder = new BinaryXmlEncoder();
+    encodeInterest(interest, encoder);
+    return new Blob(encoder.getOutput(), false);
+  }
+
+  /**
+   * Decode input as an interest in binary XML and set the fields of the interest object.
+   * @param interest The Interest object whose fields are updated.
+   * @param input The input buffer to decode.  This reads from position() to limit(), but does not change the position.
+   * @throws EncodingException For invalid encoding.
+   */
+  @Override
+  public void 
+  decodeInterest(Interest interest, ByteBuffer input) throws EncodingException
+  {
+    BinaryXmlDecoder decoder = new BinaryXmlDecoder(input);  
+    decodeInterest(interest, decoder);
+  }
+
+  private static void
+  encodeInterest(Interest interest, BinaryXmlEncoder encoder)
+  {
+    encoder.writeElementStartDTag(BinaryXml.DTag_Interest);
+
+    encodeName(interest.getName(), encoder);
+    encoder.writeOptionalUnsignedDecimalIntDTagElement(BinaryXml.DTag_MinSuffixComponents, interest.getMinSuffixComponents());
+    encoder.writeOptionalUnsignedDecimalIntDTagElement(BinaryXml.DTag_MaxSuffixComponents, interest.getMaxSuffixComponents());
+    /* TODO
+    // This will skip encoding if there is no publisherPublicKeyDigest.
+    encodePublisherPublicKeyDigest(interest.getPublisherPublicKeyDigest(), encoder);
+    // This will skip encoding if there is no exclude.
+    encodeExclude(interest.getExclude(), encoder);
+     */
+    encoder.writeOptionalUnsignedDecimalIntDTagElement(BinaryXml.DTag_ChildSelector, interest.getChildSelector());
+    if (interest.getAnswerOriginKind() >= 0 && interest.getAnswerOriginKind() != Interest.DEFAULT_ANSWER_ORIGIN_KIND)
+      encoder.writeUnsignedDecimalIntDTagElement(BinaryXml.DTag_AnswerOriginKind, interest.getAnswerOriginKind());
+    encoder.writeOptionalUnsignedDecimalIntDTagElement(BinaryXml.DTag_Scope, interest.getScope());
+    encoder.writeOptionalTimeMillisecondsDTagElement(BinaryXml.DTag_InterestLifetime, interest.getInterestLifetimeMilliseconds());
+    encoder.writeOptionalBlobDTagElement(BinaryXml.DTag_Nonce, interest.getNonce());
+
+    encoder.writeElementClose();
+  }
+
+  private static void
+  decodeInterest(Interest interest, BinaryXmlDecoder decoder) throws EncodingException
+  {
+    decoder.readElementStartDTag(BinaryXml.DTag_Interest);
+
+    decodeName(interest.getName(), decoder);
+    interest.setMinSuffixComponents(decoder.readOptionalUnsignedIntegerDTagElement(BinaryXml.DTag_MinSuffixComponents));
+    interest.setMaxSuffixComponents(decoder.readOptionalUnsignedIntegerDTagElement(BinaryXml.DTag_MaxSuffixComponents));
+    /* TODO
+    decodeOptionalPublisherPublicKeyDigest(interest.publisherPublicKeyDigest, decoder);
+    if (decoder.peekDTag(BinaryXml.DTag_Exclude))
+      decodeExclude(interest.getExclude(), decoder);
+    else
+      interest.getExclude().clear();
+     */
+    interest.setChildSelector(decoder.readOptionalUnsignedIntegerDTagElement(BinaryXml.DTag_ChildSelector));
+    interest.setAnswerOriginKind(decoder.readOptionalUnsignedIntegerDTagElement(BinaryXml.DTag_AnswerOriginKind));
+    interest.setScope(decoder.readOptionalUnsignedIntegerDTagElement(BinaryXml.DTag_Scope));
+    interest.setInterestLifetimeMilliseconds(decoder.readOptionalTimeMillisecondsDTagElement(BinaryXml.DTag_InterestLifetime));
+    interest.setNonce(new Blob(decoder.readOptionalBinaryDTagElement(BinaryXml.DTag_Nonce, false), true));
+
+    decoder.readElementClose();
+  }
+    
   private static void
   encodeName(Name name, BinaryXmlEncoder encoder)
   {
--- src/net/named_data/jndn/encoding/WireFormat.java
+++ src/net/named_data/jndn/encoding/WireFormat.java
@@ -6,6 +6,58 @@
 
 package net.named_data.jndn.encoding;
 
-public class WireFormat {
+import java.nio.ByteBuffer;
+import net.named_data.jndn.util.Blob;
+import net.named_data.jndn.Interest;
 
+public class WireFormat {
+  /**
+   * Encode interest and return the encoding.  Your derived class should override.
+   * @param interest The Interest object to encode.
+   * @return A Blob containing the encoding.
+   * @throws UnsupportedOperationException for unimplemented if the derived class does not override.
+   */
+  public Blob 
+  encodeInterest(Interest interest)
+  {
+    throw new UnsupportedOperationException("encodeInterest is not implemented");
+  }
+  
+  /**
+   * Decode input as an interest and set the fields of the interest object.  Your derived class should override.
+   * @param interest The Interest object whose fields are updated.
+   * @param input The input buffer to decode.  This reads from position() to limit(), but does not change the position.
+   * @throws UnsupportedOperationException for unimplemented if the derived class does not override.
+   * @throws EncodingException For invalid encoding.
+   */
+  public void 
+  decodeInterest(Interest interest, ByteBuffer input) throws EncodingException
+  {
+    throw new UnsupportedOperationException("decodeInterest is not implemented");
+  }
+
+  // TODO Data
+  // TODO ForwardingEntry
+  
+  /**
+   * Set the static default WireFormat used by default encoding and decoding methods.
+   * @param wireFormat An object of a subclass of WireFormat.  This does not make a copy.
+   */
+  public static void 
+  setDefaultWireFormat(WireFormat wireFormat) 
+  {
+    defaultWireFormat_ = wireFormat;
+  }
+  
+  /**
+   * Return the default WireFormat used by default encoding and decoding methods which was set with setDefaultWireFormat.
+   * @return The WireFormat object.
+   */
+  public static WireFormat
+  getDefaultWireFormat()
+  {
+    return defaultWireFormat_;
+  }
+
+  private static WireFormat defaultWireFormat_ = new BinaryXmlWireFormat();
 }
