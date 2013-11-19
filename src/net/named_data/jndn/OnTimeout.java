7765ada57ac0ffa924aac1196ac71b2b302c4032
Jeff Thompson <jefft0@gmail.com>
2013-11-18 18:43:09 -0800

    Face: Add OnTimeout interface.

---
+++ src/net/named_data/jndn/OnTimeout.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (C) 2013 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * See COPYING for copyright and distribution information.
+ */
+
+package net.named_data.jndn;
+
+/**
+ * A class implements OnTimeout if it has onTimeout, used to pass a callback to expressInterest.
+ * @author jefft0
+ */
+public interface OnTimeout {
+  /**
+   * If the interest times out according to the interest lifetime, onTimeout is called.
+   * @param interest The interest given to expressInterest.
+   */
+  void onTimeout(Interest interest);
+}
