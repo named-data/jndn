a49725f0a5bded5b4b4309ea0ba614ab10257b69
Jeff Thompson <jefft0@gmail.com>
2013-11-18 18:42:48 -0800

    Face: Add OnData interface.

---
+++ src/net/named_data/jndn/OnData.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (C) 2013 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * See COPYING for copyright and distribution information.
+ */
+
+package net.named_data.jndn;
+
+/**
+ * A class implements OnData if it has onData, used to pass a callback to expressInterest.
+ */
+public interface OnData {
+  /**
+   * When a matching data packet is received, onData is called.
+   * @param interest The interest given to Face.expressInterest.
+   * @param data The received Data object.
+   */
+  void onData(Interest interest, Data data);
+}
