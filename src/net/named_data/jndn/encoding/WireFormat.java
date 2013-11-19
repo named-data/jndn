3307db98b18e1553b1a421cf087d5032de43a79d
Jeff Thompson <jefft0@gmail.com>
2013-11-19 14:26:11 -0800

    Add encode/decodeData.

--- src/net/named_data/jndn/encoding/WireFormat.java
+++ src/net/named_data/jndn/encoding/WireFormat.java
@@ -9,6 +9,7 @@ package net.named_data.jndn.encoding;
 import java.nio.ByteBuffer;
 import net.named_data.jndn.util.Blob;
 import net.named_data.jndn.Interest;
+import net.named_data.jndn.Data;
 
 public class WireFormat {
   /**
@@ -36,7 +37,68 @@ public class WireFormat {
     throw new UnsupportedOperationException("decodeInterest is not implemented");
   }
 
-  // TODO Data
+  /**
+   * Encode data and return the encoding.  Your derived class should override.
+   * @param data The Data object to encode.
+   * @param signedPortionBeginOffset Return the offset in the encoding of the beginning of the signed portion by
+   * setting signedPortionBeginOffset[0].
+   * If you are not encoding in order to sign, you can call encodeData(data) to ignore this returned value.
+   * @param signedPortionEndOffset Return the offset in the encoding of the end of the signed portion by
+   * setting signedPortionEndOffset[0].
+   * If you are not encoding in order to sign, you can call encodeData(data) to ignore this returned value.
+   * @return A Blob containing the encoding.
+   * @throws UnsupportedOperationException for unimplemented if the derived class does not override.
+   */
+  public Blob 
+  encodeData(Data data, int[] signedPortionBeginOffset, int[] signedPortionEndOffset)
+  {
+    throw new UnsupportedOperationException("encodeData is not implemented");
+  }
+
+  /**
+   * Encode data and return the encoding.
+   * @param data The Data object to encode.
+   * @return A Blob containing the encoding.
+   * @throws UnsupportedOperationException for unimplemented if the derived class does not override.
+   */
+  public final Blob 
+  encodeData(Data data)
+  {
+    return encodeData(data, new int[1], new int[1]);
+  }
+
+  /**
+   * Decode input as a data packet and set the fields in the data object.  Your derived class should override.
+   * @param data The Data object whose fields are updated.
+   * @param input The input buffer to decode.  This reads from position() to limit(), but does not change the position.
+   * @param signedPortionBeginOffset Return the offset in the input buffer of the beginning of the signed portion by
+   * setting signedPortionBeginOffset[0].  If you are not decoding in order to verify, you can call 
+   * decodeData(data, input) to ignore this returned value.
+   * @param signedPortionEndOffset Return the offset in the input buffer of the end of the signed portion by
+   * setting signedPortionEndOffset[0]. If you are not decoding in order to verify, you can call 
+   * decodeData(data, input) to ignore this returned value.
+   * @throws UnsupportedOperationException for unimplemented if the derived class does not override.
+   * @throws EncodingException For invalid encoding.
+   */
+  public void 
+  decodeData(Data data, ByteBuffer input, int[] signedPortionBeginOffset, int[] signedPortionEndOffset) throws EncodingException
+  {
+    throw new UnsupportedOperationException("decodeData is not implemented");
+  }
+
+  /**
+   * Decode input as a data packet and set the fields in the data object.  Your derived class should override.
+   * @param data The Data object whose fields are updated.
+   * @param input The input buffer to decode.  This reads from position() to limit(), but does not change the position.
+   * @throws UnsupportedOperationException for unimplemented if the derived class does not override.
+   * @throws EncodingException For invalid encoding.
+   */
+  public final void 
+  decodeData(Data data, ByteBuffer input) throws EncodingException
+  {
+    decodeData(data, input, new int[1], new int[1]);
+  }
+  
   // TODO ForwardingEntry
   
   /**
