d25c7386b2ea2fad27b1ac3fd55943ff9b96128f
Jeff Thompson <jefft0@gmail.com>
2013-11-18 18:57:17 -0800

    Face: Added OnRegisterFailed interface.

---
+++ src/net/named_data/jndn/OnRegisterFailed.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (C) 2013 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * See COPYING for copyright and distribution information.
+ */
+
+package net.named_data.jndn;
+
+/**
+ * A class implements OnRegisterFailed if it has onRegisterFailed, used to pass a callback to Face.registerPrefix.
+ */
+public interface OnRegisterFailed {
+  /**
+   * If failed to retrieve the connected hub's ID or failed to register the prefix, onRegisterFailed is called.
+   * @param prefix The prefix given to registerPrefix.
+   */
+  void onRegisterFailed(Name prefix);
+}
