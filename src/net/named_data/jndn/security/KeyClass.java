3e14a0582854eb3cde69e8938a4e93d1bba3d375
Jeff Thompson <jefft0@gmail.com>
2013-12-13 12:51:28 -0800

    security: In KeyClass, simlify the enum values.

--- src/net/named_data/jndn/security/KeyClass.java
+++ src/net/named_data/jndn/security/KeyClass.java
@@ -1,13 +1,14 @@
 /**
  * Copyright (C) 2013 Regents of the University of California.
  * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * @author: From code in NDN-CPP by Yingdi Yu <yingdi@cs.ucla.edu>
  * See COPYING for copyright and distribution information.
  */
 
 package net.named_data.jndn.security;
 
 public enum KeyClass {
-  KEY_CLASS_PUBLIC,
-  KEY_CLASS_PRIVATE,
-  KEY_CLASS_SYMMETRIC
+  PUBLIC,
+  PRIVATE,
+  SYMMETRIC
 }
