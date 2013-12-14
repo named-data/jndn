dba952016d74c89db77f3652c4424bb94b7aa074
Jeff Thompson <jefft0@gmail.com>
2013-12-13 19:06:03 -0800

    security: Added stubs for OID and DerNode.

---
+++ src/net/named_data/jndn/encoding/der/DerNode.java
@@ -0,0 +1,12 @@
+/**
+ * Copyright (C) 2013 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * @author: From code in NDN-CPP by Yingdi Yu <yingdi@cs.ucla.edu>
+ * See COPYING for copyright and distribution information.
+ */
+
+package net.named_data.jndn.encoding.der;
+
+public class DerNode {
+  // TODO: Implement DerNode.
+}
