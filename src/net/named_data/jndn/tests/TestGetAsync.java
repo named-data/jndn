5c9a71d2ccbc2f7d83f96914fdbe0cab9ea14502
Jeff Thompson <jefft0@gmail.com>
2014-03-20 21:30:10 -0700

    tests: Update TestGetAsync to use Face.

--- src/net/named_data/jndn/tests/TestGetAsync.java
+++ src/net/named_data/jndn/tests/TestGetAsync.java
@@ -8,14 +8,13 @@ package net.named_data.jndn.tests;
 
 import java.nio.ByteBuffer;
 import net.named_data.jndn.Data;
+import net.named_data.jndn.Face;
 import net.named_data.jndn.Interest;
 import net.named_data.jndn.Name;
-import net.named_data.jndn.Node;
 import net.named_data.jndn.OnData;
-import net.named_data.jndn.encoding.TlvWireFormat;
-import net.named_data.jndn.transport.TcpTransport;
+import net.named_data.jndn.OnTimeout;
 
-class Counter implements OnData {
+class Counter implements OnData, OnTimeout {
   public void
   onData(Interest interest, Data data) 
   {
@@ -29,6 +28,12 @@ class Counter implements OnData {
   }
   
   public int callbackCount_ = 0;
+
+  public void onTimeout(Interest interest) 
+  {
+    ++callbackCount_;
+    System.out.println("Time out for interest " + interest.getName().toUri());
+  }
 }
 
 public class TestGetAsync {
@@ -36,34 +41,34 @@ public class TestGetAsync {
   main(String[] args) 
   {
     try {
-      // Face face("borges.metwi.ucla.edu");
-      Node node = new Node
-       (new TcpTransport(), new TcpTransport.ConnectionInfo("borges.metwi.ucla.edu"));
+      Face face = new Face("borges.metwi.ucla.edu");
       
       Counter counter = new Counter();
 
-      Name name1 = new Name("/");
+      Name name1 = new Name("/ndn/edu/ucla/remap/ndn-js-test/howdy.txt/%FD%052%A1%DF%5E%A4");
       System.out.println("Express name " + name1.toUri());
-      // face.expressInterest
-      //   (name1, bind(&Counter::onData, &counter, _1, _2), 
-      //    bind(&Counter::onTimeout, &counter, _1));
-      {
-        Interest interest = new Interest(name1, 4000.0);
-        node.expressInterest(interest, counter, null, TlvWireFormat.get());
-      }
+      face.expressInterest(name1, counter, counter); 
+
+      // Try to get anything.
+      Name name2 = new Name("/");
+      System.out.println("Express name " + name2.toUri());
+      face.expressInterest(name2, counter, counter); 
+
+      // Expect this to time out.
+      Name name3 = new Name("/test/timeout");
+      System.out.println("Express name " + name3.toUri());
+      face.expressInterest(name3, counter, counter); 
 
       // The main event loop.
-      while (counter.callbackCount_ < 1) {
-        //face.processEvents();
-        {
-          node.processEvents();
-        }
+      while (counter.callbackCount_ < 3) {
+        face.processEvents();
+
         // We need to sleep for a few milliseconds so we don't use 100% of 
         //   the CPU.
         Thread.sleep(5);
       }
     }
-    catch (Throwable e) {
+    catch (Exception e) {
        System.out.println("exception: " + e.getMessage());
     }
   }
