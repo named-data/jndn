82a4ff4f52b71359736adc31d9a6bb68333d9803
Jeff Thompson <jefft0@gmail.com>
2014-03-26 16:37:58 -0700

    security: Added author attribution.

--- src/net/named_data/jndn/security/UnrecognizedKeyFormatException.java
+++ src/net/named_data/jndn/security/UnrecognizedKeyFormatException.java
@@ -1,6 +1,7 @@
 /**
  * Copyright (C) 2014 Regents of the University of California.
  * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * @author: From code in NDN-CPP by Yingdi Yu <yingdi@cs.ucla.edu>
  * See COPYING for copyright and distribution information.
  */
 
