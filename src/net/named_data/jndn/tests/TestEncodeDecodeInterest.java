fd13ea22f81078c832b501875c9a80cf4b230525
Jeff Thompson <jefft0@gmail.com>
2014-05-14 12:46:13 -0700

    tests: In TestEncodeDecodeInterest, fix the KeyLocator type code in TlvInterest.

--- src/net/named_data/jndn/tests/TestEncodeDecodeInterest.java
+++ src/net/named_data/jndn/tests/TestEncodeDecodeInterest.java
@@ -53,7 +53,7 @@ public class TestEncodeDecodeInterest {
   0x09, 0x38, // Selectors
     0x0D, 0x01, 0x04, // MinSuffixComponents
     0x0E, 0x01, 0x06, // MaxSuffixComponents
-    0x1C, 0x22, // KeyLocator
+    0x0F, 0x22, // KeyLocator
       0x1D, 0x20, // KeyLocatorDigest
                   0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
                   0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F,
@@ -141,7 +141,7 @@ public class TestEncodeDecodeInterest {
       freshInterest.getExclude().appendComponent(new Name("abc").get(0)).appendAny();
       freshInterest.setInterestLifetimeMilliseconds(30000);
       freshInterest.setChildSelector(1);
-      freshInterest.setMustBeFresh(false);
+      freshInterest.setMustBeFresh(true);
       freshInterest.setScope(2);
 
       Interest reDecodedFreshInterest = new Interest();
