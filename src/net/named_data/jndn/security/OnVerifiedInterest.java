7b5edb14377e4f9eb2e495213c4690d72013fe60
Jeff Thompson <jefft0@remap.ucla.edu>
2014-09-12 09:44:00 -0700

    security: Added OnVerifiedInterest and OnVerifyInterestFailed.

---
+++ src/net/named_data/jndn/security/OnVerifiedInterest.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (C) 2014 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * @author: From code in NDN-CPP by Yingdi Yu <yingdi@cs.ucla.edu>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU General Public License is in the file COPYING.
+ */
+
+package net.named_data.jndn.security;
+
+import net.named_data.jndn.Interest;
+
+/**
+ * A class implements OnVerifiedInterest if it has onVerifiedInterest which is
+ * called by verifyInterest to report a successful verification.
+ */
+public interface OnVerifiedInterest {
+  /**
+   * When verifyInterest succeeds, onVerifiedInterest is called.
+   * @param interest The interest object being verified.
+   */
+  void onVerifiedInterest(Interest interest);
+}
