a8663c75ed9486bbda7ccb96c2bb58bfb4579646
Jeff Thompson <jefft0@remap.ucla.edu>
2014-10-10 11:21:31 -0700

    security: In BasicIdentityStorage, implemented getDefaultCertificateNameForKey.

--- src/net/named_data/jndn/security/identity/BasicIdentityStorage.java
+++ src/net/named_data/jndn/security/identity/BasicIdentityStorage.java
@@ -24,6 +24,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -41,7 +42,7 @@ import net.named_data.jndn.util.Blob;
  * identity, public keys and certificates using SQLite.
  */
 public class BasicIdentityStorage extends IdentityStorage {
-  public BasicIdentityStorage() throws SQLException
+  public BasicIdentityStorage() throws SecurityException
   {
     try {
       Class.forName("org.sqlite.JDBC");
@@ -54,49 +55,58 @@ public class BasicIdentityStorage extends IdentityStorage {
     Path identityDir = Paths.get(System.getProperty("user.home", "."), ".ndn");
     identityDir.toFile().mkdirs();
 
-    Path databasePath = identityDir.resolve("ndnsec-public-info.db");
-    database_ = DriverManager.getConnection("jdbc:sqlite:" + databasePath);
-
-    Statement statement = database_.createStatement();
-    // Use "try/finally instead of "try-with-resources" or "using" which are not supported before Java 7.
     try {
-      //Check if the ID table exists.
-      ResultSet result = statement.executeQuery
-        ("SELECT name FROM sqlite_master WHERE type='table' And name='Identity'");
-      boolean idTableExists = false;
-      if (result.next())
-        idTableExists = true;      
-      result.close();
+      Path databasePath = identityDir.resolve("ndnsec-public-info.db");
+      database_ = DriverManager.getConnection("jdbc:sqlite:" + databasePath);
 
-      if (!idTableExists)
-        statement.executeUpdate(INIT_ID_TABLE);
+      Statement statement = database_.createStatement();
+      // Use "try/finally instead of "try-with-resources" or "using" which are not supported before Java 7.
+      try {
+        //Check if the ID table exists.
+        ResultSet result = statement.executeQuery
+          ("SELECT name FROM sqlite_master WHERE type='table' And name='Identity'");
+        boolean idTableExists = false;
+        if (result.next())
+          idTableExists = true;
+        result.close();
 
-      //Check if the Key table exists.
-      result = statement.executeQuery
-        ("SELECT name FROM sqlite_master WHERE type='table' And name='Key'");
-      idTableExists = false;
-      if (result.next())
-        idTableExists = true;
-      result.close();
+        if (!idTableExists)
+          statement.executeUpdate(INIT_ID_TABLE);
 
-      if (!idTableExists)
-        statement.executeUpdate(INIT_KEY_TABLE);
+        //Check if the Key table exists.
+        result = statement.executeQuery
+          ("SELECT name FROM sqlite_master WHERE type='table' And name='Key'");
+        idTableExists = false;
+        if (result.next())
+          idTableExists = true;
+        result.close();
 
-      //Check if the Certificate table exists.
-      result = statement.executeQuery
-        ("SELECT name FROM sqlite_master WHERE type='table' And name='Certificate'");
-      idTableExists = false;
-      if (result.next())
-        idTableExists = true;
-      result.close();
+        if (!idTableExists)
+          statement.executeUpdate(INIT_KEY_TABLE);
 
-      if (!idTableExists)
-        statement.executeUpdate(INIT_CERT_TABLE);
-    } finally {
-      statement.close();
+        //Check if the Certificate table exists.
+        result = statement.executeQuery
+          ("SELECT name FROM sqlite_master WHERE type='table' And name='Certificate'");
+        idTableExists = false;
+        if (result.next())
+          idTableExists = true;
+        result.close();
+
+        if (!idTableExists)
+          statement.executeUpdate(INIT_CERT_TABLE);
+      } finally {
+        statement.close();
+      }
+    } catch (SQLException exception) {
+      throw new SecurityException("BasicIdentityStorageSQLite error: " + exception);
     }
   }
 
+  /**
+   * Check if the specified identity already exists.
+   * @param identityName The identity name.
+   * @return True if the identity exists, otherwise false.
+   */
   public final boolean
   doesIdentityExist(Name identityName)
   {
@@ -104,6 +114,12 @@ public class BasicIdentityStorage extends IdentityStorage {
       ("BasicIdentityStorage.doesIdentityExist is not implemented");
   }
 
+  /**
+   * Add a new identity. An exception will be thrown if the identity already
+   * exists.
+   * @param identityName The identity name to be added.
+   * @throws SecurityException if the identityName is already added.
+   */
   public final void
   addIdentity(Name identityName) throws SecurityException
   {
@@ -111,6 +127,10 @@ public class BasicIdentityStorage extends IdentityStorage {
       ("BasicIdentityStorage.addIdentity is not implemented");
   }
 
+  /**
+   * Revoke the identity.
+   * @return True if the identity was revoked, false if not.
+   */
   public final boolean
   revokeIdentity()
   {
@@ -118,6 +138,11 @@ public class BasicIdentityStorage extends IdentityStorage {
       ("BasicIdentityStorage.revokeIdentity is not implemented");
   }
 
+  /**
+   * Check if the specified key already exists.
+   * @param keyName The name of the key.
+   * @return true if the key exists, otherwise false.
+   */
   public final boolean
   doesKeyExist(Name keyName)
   {
@@ -125,6 +150,13 @@ public class BasicIdentityStorage extends IdentityStorage {
       ("BasicIdentityStorage.doesKeyExist is not implemented");
   }
 
+  /**
+   * Add a public key to the identity storage.
+   * @param keyName The name of the public key to be added.
+   * @param keyType Type of the public key to be added.
+   * @param publicKeyDer A blob of the public key DER to be added.
+   * @throws SecurityException if a key with the keyName already exists.
+   */
   public final void
   addKey(Name keyName, KeyType keyType, Blob publicKeyDer) throws SecurityException
   {
@@ -132,6 +164,11 @@ public class BasicIdentityStorage extends IdentityStorage {
       ("BasicIdentityStorage.addKey is not implemented");
   }
 
+  /**
+   * Get the public key DER blob from the identity storage.
+   * @param keyName The name of the requested public key.
+   * @return The DER Blob.  If not found, return a Blob with a null pointer.
+   */
   public final Blob
   getKey(Name keyName)
   {
@@ -139,6 +176,12 @@ public class BasicIdentityStorage extends IdentityStorage {
       ("BasicIdentityStorage.getKey is not implemented");
   }
 
+  /**
+   * Get the KeyType of the public key with the given keyName.
+   * @param keyName The name of the requested public key.
+   * @return The KeyType, for example KeyType.RSA.
+   * @throws SecurityException if the keyName is not found.
+   */
   public final KeyType
   getKeyType(Name keyName) throws SecurityException
   {
@@ -146,6 +189,11 @@ public class BasicIdentityStorage extends IdentityStorage {
       ("BasicIdentityStorage.getKeyType is not implemented");
   }
 
+  /**
+   * Activate a key.  If a key is marked as inactive, its private part will not
+   * be used in packet signing.
+   * @param keyName The name of the key.
+   */
   public final void
   activateKey(Name keyName)
   {
@@ -153,6 +201,11 @@ public class BasicIdentityStorage extends IdentityStorage {
       ("BasicIdentityStorage.activateKey is not implemented");
   }
 
+  /**
+   * Deactivate a key. If a key is marked as inactive, its private part will not
+   * be used in packet signing.
+   * @param keyName The name of the key.
+   */
   public final void
   deactivateKey(Name keyName)
   {
@@ -160,6 +213,11 @@ public class BasicIdentityStorage extends IdentityStorage {
       ("BasicIdentityStorage.deactivateKey is not implemented");
   }
 
+  /**
+   * Check if the specified certificate already exists.
+   * @param certificateName The name of the certificate.
+   * @return True if the certificate exists, otherwise false.
+   */
   public final boolean
   doesCertificateExist(Name certificateName)
   {
@@ -167,6 +225,12 @@ public class BasicIdentityStorage extends IdentityStorage {
       ("BasicIdentityStorage.doesCertificateExist is not implemented");
   }
 
+  /**
+   * Add a certificate to the identity storage.
+   * @param certificate The certificate to be added.  This makes a copy of the
+   * certificate.
+   * @throws SecurityException if the certificate is already installed.
+   */
   public final void
   addCertificate(IdentityCertificate certificate) throws SecurityException
   {
@@ -174,6 +238,13 @@ public class BasicIdentityStorage extends IdentityStorage {
       ("BasicIdentityStorage.addCertificate is not implemented");
   }
 
+  /**
+   * Get a certificate from the identity storage.
+   * @param certificateName The name of the requested certificate.
+   * @param allowAny If false, only a valid certificate will be
+   * returned, otherwise validity is disregarded.
+   * @return The requested certificate. If not found, return null.
+   */
   public final Data
   getCertificate(Name certificateName, boolean allowAny)
   {
@@ -185,27 +256,103 @@ public class BasicIdentityStorage extends IdentityStorage {
    *           Get/Set Default             *
    *****************************************/
 
+  /**
+   * Get the default identity.
+   * @return The name of default identity.
+   * @throws SecurityException if the default identity is not set.
+   */
   public final Name
   getDefaultIdentity() throws SecurityException
   {
-    throw new UnsupportedOperationException
-      ("BasicIdentityStorage.getDefaultIdentity is not implemented");
+    try {
+      Statement statement = database_.createStatement();
+      try {
+        ResultSet result = statement.executeQuery
+          ("SELECT identity_name FROM Identity WHERE default_identity=1");
+
+        if (result.next())
+          return new Name(result.getString("identity_name"));
+        else
+          throw new SecurityException
+            ("BasicIdentityStorage::getDefaultIdentity: The default identity is not defined");
+      } finally {
+        statement.close();
+      }
+    } catch (SQLException exception) {
+      throw new SecurityException("BasicIdentityStorageSQLite error: " + exception);
+    }
   }
 
+  /**
+   * Get the default key name for the specified identity.
+   * @param identityName The identity name.
+   * @return The default key name.
+   * @throws SecurityException if the default key name for the identity is not set.
+   */
   public final Name
   getDefaultKeyNameForIdentity(Name identityName) throws SecurityException
   {
-    throw new UnsupportedOperationException
-      ("BasicIdentityStorage.getDefaultKeyNameForIdentity is not implemented");
+    try {
+      PreparedStatement statement = database_.prepareStatement
+        ("SELECT key_identifier FROM Key WHERE identity_name=? AND default_key=1");
+      statement.setString(1, identityName.toUri());
+
+      try {
+        ResultSet result = statement.executeQuery();
+
+        if (result.next())
+          return new Name(identityName).append(result.getString("key_identifier"));
+        else
+          throw new SecurityException
+            ("BasicIdentityStorage::getDefaultKeyNameForIdentity: The default key for the identity is not defined");
+      } finally {
+        statement.close();
+      }
+    } catch (SQLException exception) {
+      throw new SecurityException("BasicIdentityStorageSQLite error: " + exception);
+    }
   }
 
+  /**
+   * Get the default certificate name for the specified key.
+   * @param keyName The key name.
+   * @return The default certificate name.
+   * @throws SecurityException if the default certificate name for the key name
+   * is not set.
+   */
   public final Name
   getDefaultCertificateNameForKey(Name keyName) throws SecurityException
   {
-    throw new UnsupportedOperationException
-      ("BasicIdentityStorage.getDefaultCertificateNameForKey is not implemented");
+    String keyId = keyName.get(-1).toEscapedString();
+    Name identityName = keyName.getSubName(0, keyName.size() - 1);
+
+    try {
+      PreparedStatement statement = database_.prepareStatement
+        ("SELECT cert_name FROM Certificate WHERE identity_name=? AND key_identifier=? AND default_cert=1");
+      statement.setString(1, identityName.toUri());
+      statement.setString(2, keyId);
+
+      try {
+        ResultSet result = statement.executeQuery();
+
+        if (result.next())
+          return new Name(result.getString("cert_name"));
+        else
+          throw new SecurityException
+            ("BasicIdentityStorage::getDefaultCertificateNameForKey: The default certificate for the key name is not defined");
+      } finally {
+        statement.close();
+      }
+    } catch (SQLException exception) {
+      throw new SecurityException("BasicIdentityStorageSQLite error: " + exception);
+    }
   }
 
+  /**
+   * Set the default identity.  If the identityName does not exist, then clear
+   * the default identity so that getDefaultIdentity() throws an exception.
+   * @param identityName The default identity name.
+   */
   public final void
   setDefaultIdentity(Name identityName)
   {
@@ -213,6 +360,11 @@ public class BasicIdentityStorage extends IdentityStorage {
       ("BasicIdentityStorage.setDefaultIdentity is not implemented");
   }
 
+  /**
+   * Set the default key name for the specified identity.
+   * @param keyName The key name.
+   * @param identityNameCheck The identity name to check the keyName.
+   */
   public final void
   setDefaultKeyNameForIdentity(Name keyName, Name identityNameCheck)
   {
@@ -220,6 +372,11 @@ public class BasicIdentityStorage extends IdentityStorage {
       ("BasicIdentityStorage.setDefaultKeyNameForIdentity is not implemented");
   }
 
+  /**
+   * Set the default key name for the specified identity.
+   * @param keyName The key name.
+   * @param certificateName The certificate name.
+   */
   public final void
   setDefaultCertificateNameForKey(Name keyName, Name certificateName)
   {
@@ -227,7 +384,7 @@ public class BasicIdentityStorage extends IdentityStorage {
       ("BasicIdentityStorage.setDefaultCertificateNameForKey is not implemented");
   }
 
-  static final String INIT_ID_TABLE =
+  private static final String INIT_ID_TABLE =
 "CREATE TABLE IF NOT EXISTS                                           \n" +
 "  Identity(                                                          \n" +
 "      identity_name     BLOB NOT NULL,                               \n" +
@@ -238,7 +395,7 @@ public class BasicIdentityStorage extends IdentityStorage {
 "                                                                     \n" +
 "CREATE INDEX identity_index ON Identity(identity_name);              \n";
 
-  static final String INIT_KEY_TABLE =
+  private static final String INIT_KEY_TABLE =
 "CREATE TABLE IF NOT EXISTS                                           \n" +
 "  Key(                                                               \n" +
 "      identity_name     BLOB NOT NULL,                               \n" +
@@ -253,7 +410,7 @@ public class BasicIdentityStorage extends IdentityStorage {
 "                                                                     \n" +
 "CREATE INDEX key_index ON Key(identity_name);                        \n";
 
-  static final String INIT_CERT_TABLE =
+  private static final String INIT_CERT_TABLE =
 "CREATE TABLE IF NOT EXISTS                                           \n" +
 "  Certificate(                                                       \n" +
 "      cert_name         BLOB NOT NULL,                               \n" +
