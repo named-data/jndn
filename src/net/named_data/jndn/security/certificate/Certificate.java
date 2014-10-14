b4e0fbca643467fcc9b8e3fcb29d0ebfeb60ef57
Jeff Thompson <jefft0@remap.ucla.edu>
2014-10-14 11:06:35 -0700

    security: In Certificate, add stubs for all methods.

--- src/net/named_data/jndn/security/certificate/Certificate.java
+++ src/net/named_data/jndn/security/certificate/Certificate.java
@@ -20,8 +20,149 @@
 
 package net.named_data.jndn.security.certificate;
 
+import java.util.List;
 import net.named_data.jndn.Data;
+import net.named_data.jndn.Name;
 
-public class Certificate extends Data {
-  // TODO: Implement Certificate.
+public abstract class Certificate extends Data {
+  /**
+   * The default constructor.
+   */
+  public Certificate()
+  {
+    throw new UnsupportedOperationException
+      ("Certificate constructor is not implemented");
+  }
+
+  /**
+   * Create a Certificate from the content in the data packet.
+   * @param data The data packet with the content to decode.
+   */
+  public Certificate(Data data)
+  {
+    throw new UnsupportedOperationException
+      ("Certificate constructor is not implemented");
+  }
+
+  /**
+   * encode certificate info into content
+   */
+  public final void
+  encode()
+  {
+    throw new UnsupportedOperationException
+      ("Certificate.encode is not implemented");
+  }
+
+  /**
+   * Add a subject description.
+   * @param description The description to be added.
+   */
+  public final void
+  addSubjectDescription(CertificateSubjectDescription description)
+  {
+    throw new UnsupportedOperationException
+      ("Certificate.addSubjectDescription is not implemented");
+  }
+
+  // List of CertificateSubjectDescription.
+  public final List
+  getSubjectDescriptionList()
+  {
+    throw new UnsupportedOperationException
+      ("Certificate.getSubjectDescriptionList is not implemented");
+  }
+
+  /**
+   * Add a certificate extension.
+   * @param extension the extension to be added
+   */
+  public final void
+  addExtension(CertificateExtension extension)
+  {
+    throw new UnsupportedOperationException
+      ("Certificate.addExtension is not implemented");
+  }
+
+  // List of CertificateExtension.
+  public final List
+  getExtensionList()
+  {
+    throw new UnsupportedOperationException
+      ("Certificate.getExtensionList is not implemented");
+  }
+
+  public final void
+  setNotBefore(double notBefore)
+  {
+    throw new UnsupportedOperationException
+      ("Certificate.setNotBefore is not implemented");
+  }
+
+  public final double
+  getNotBefore()
+  {
+    throw new UnsupportedOperationException
+      ("Certificate.getNotBefore is not implemented");
+  }
+
+  public final void
+  setNotAfter(double notAfter)
+  {
+    throw new UnsupportedOperationException
+      ("Certificate.setNotAfter is not implemented");
+  }
+
+  public final double
+  getNotAfter()
+  {
+    throw new UnsupportedOperationException
+      ("Certificate.getNotAfter is not implemented");
+  }
+
+  public final void
+  setPublicKeyInfo(PublicKey key)
+  {
+    throw new UnsupportedOperationException
+      ("Certificate.setPublicKeyInfo is not implemented");
+  }
+
+  public final PublicKey
+  getPublicKeyInfo()
+  {
+    throw new UnsupportedOperationException
+      ("Certificate.getPublicKeyInfo is not implemented");
+  }
+
+  public abstract Name
+  getPublicKeyName();
+
+  /**
+   * Check if the certificate is valid.
+   * @return True if the current time is earlier than notBefore.
+   */
+  public final boolean
+  isTooEarly()
+  {
+    throw new UnsupportedOperationException
+      ("Certificate.isTooEarly is not implemented");
+  }
+
+  /**
+   * Check if the certificate is valid.
+   * @return True if the current time is later than notAfter.
+   */
+  public final boolean
+  isTooLate()
+  {
+    throw new UnsupportedOperationException
+      ("Certificate.isTooLate is not implemented");
+  }
+
+  public final void
+  printCertificate()
+  {
+    throw new UnsupportedOperationException
+      ("Certificate.printCertificate is not implemented");
+  }
 }
