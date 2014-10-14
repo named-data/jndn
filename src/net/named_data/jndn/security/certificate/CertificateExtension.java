f50185a6da95eb03a8bd6770ebb88b7fe42888ec
Jeff Thompson <jefft0@remap.ucla.edu>
2014-10-14 11:05:24 -0700

    security: Implement CertificateSubjectDescription and CertificateExtension (except for toDer).

---
+++ src/net/named_data/jndn/security/certificate/CertificateExtension.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (C) 2014 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * @author: From code in NDN-CPP by Yingdi Yu <yingdi@cs.ucla.edu>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+package net.named_data.jndn.security.certificate;
+
+import net.named_data.jndn.encoding.OID;
+import net.named_data.jndn.encoding.der.DerNode;
+import net.named_data.jndn.util.Blob;
+
+/**
+ * A CertificateExtension represents the Extension entry in a certificate.
+ */
+public class CertificateExtension {
+  /**
+   * Create a new CertificateExtension.
+   * @param oid The oid of subject description entry.
+   * @param isCritical If true, the extension must be handled.
+   * @param value The extension value.
+   */
+  public CertificateExtension(String oid, boolean isCritical, Blob value)
+  {
+    extensionId_ = new OID(oid);
+    isCritical_ = isCritical;
+    extensionValue_ = value;
+  }
+
+  /**
+   * Create a new CertificateExtension.
+   * @param oid The oid of subject description entry.
+   * @param isCritical If true, the extension must be handled.
+   * @param value The extension value.
+   */
+  public CertificateExtension(OID oid, boolean isCritical, Blob value)
+  {
+    extensionId_ = oid;
+    isCritical_ = isCritical;
+    extensionValue_ = value;
+  }
+
+  /**
+   * Encode the object into DER syntax tree.
+   * @return The encoded DER syntax tree.
+   */
+  public final DerNode
+  toDer()
+  {
+    throw new UnsupportedOperationException
+      ("CertificateExtension.toDer is not implemented");
+  }
+
+  public final Blob
+  toDerBlob()
+  {
+    throw new UnsupportedOperationException
+      ("CertificateExtension.toDerBlob is not implemented");
+  }
+
+  public final OID
+  getOid() { return extensionId_; }
+
+  public final boolean
+  getIsCritical() { return isCritical_; }
+
+  public final Blob
+  getValue() { return extensionValue_; }
+
+  protected final OID extensionId_;
+  protected final boolean isCritical_;
+  protected final Blob extensionValue_;
+}
---
+++ src/net/named_data/jndn/security/certificate/CertificateSubjectDescription.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (C) 2014 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * @author: From code in NDN-CPP by Yingdi Yu <yingdi@cs.ucla.edu>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+package net.named_data.jndn.security.certificate;
+
+import net.named_data.jndn.encoding.OID;
+import net.named_data.jndn.encoding.der.DerNode;
+import net.named_data.jndn.util.Blob;
+
+/**
+ * A CertificateSubjectDescription represents the SubjectDescription entry in a
+ * Certificate.
+ */
+public class CertificateSubjectDescription {
+  /**
+   * Create a new CertificateSubjectDescription.
+   * @param oid The oid of the subject description entry.
+   * @param value The value of the subject description entry.
+   */
+  public CertificateSubjectDescription(String oid, String value)
+  {
+    oid_ = new OID(oid);
+    value_ = value;
+  }
+
+  /**
+   * Create a new CertificateSubjectDescription.
+   * @param oid The oid of the subject description entry.
+   * @param value The value of the subject description entry.
+   */
+  public CertificateSubjectDescription(OID oid, String value)
+  {
+    oid_ = oid;
+    value_ = value;
+  }
+
+  /**
+   * Encode the object into a DER syntax tree.
+   * @return The encoded DER syntax tree.
+   */
+  public final DerNode
+  toDer()
+  {
+    throw new UnsupportedOperationException
+      ("CertificateSubjectDescription.toDer is not implemented");
+  }
+
+  public final String
+  getOidString()
+  {
+    return oid_.toString();
+  }
+
+  public final String
+  getValue()
+  {
+    return value_;
+  }
+
+  private final OID oid_;
+  private final String value_;
+}
