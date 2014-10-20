0d4230efa1d1a3d94bc7adcdf95898ef8091a82b
Jeff Thompson <jefft0@remap.ucla.edu>
2014-10-20 10:47:30 -0700

    encoding (minor): In DerNode, fix author comment.

--- src/net/named_data/jndn/encoding/der/DerNode.java
+++ src/net/named_data/jndn/encoding/der/DerNode.java
@@ -1,7 +1,7 @@
 /**
  * Copyright (C) 2013-2014 Regents of the University of California.
  * @author: Jeff Thompson <jefft0@remap.ucla.edu>
- * @author: From PyNDN der.py by Adeola Bannis <thecodemaiden@gmail.com>.
+ * @author: From PyNDN der_node.py by Adeola Bannis <thecodemaiden@gmail.com>.
  * @author: Originally from code in NDN-CPP by Yingdi Yu <yingdi@cs.ucla.edu>
  *
  * This program is free software: you can redistribute it and/or modify
