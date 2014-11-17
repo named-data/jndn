1560368f67a119e4035a8ddb140441d83f25d39a
Jeff Thompson <jefft0@remap.ucla.edu>
2014-11-17 10:38:44 -0800

    encoding (minor): Make DerNode.decode protected.

--- src/net/named_data/jndn/encoding/der/DerNode.java
+++ src/net/named_data/jndn/encoding/der/DerNode.java
@@ -154,7 +154,7 @@ public class DerNode {
    * position.
    * @param startIdx The offset into the buffer.
    */
-  public void
+  protected void
   decode(ByteBuffer inputBuf, int startIdx) throws DerDecodingException
   {
     int idx = startIdx;
