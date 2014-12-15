3bcf2a640a5f6e103d37310abd0e1fb31b227731
Jeff Thompson <jefft0@remap.ucla.edu>
2014-12-15 15:50:53 -0800

    encoding: Added Tlv0_1WireFormat as an alias for Tlv0_1_1WireFormat.

---
+++ src/net/named_data/jndn/encoding/Tlv0_1WireFormat.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (C) 2014 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+package net.named_data.jndn.encoding;
+
+/**
+ * A Tlv0_1WireFormat extends Tlv0_1_1WireFormat so that it is an alias in case
+ * any applications use Tlv0_1WireFormat directly.  These two wire formats are
+ * the same except that Tlv0_1_1WireFormat adds support for
+ * Sha256WithEcdsaSignature.
+ */
+public class Tlv0_1WireFormat extends Tlv0_1_1WireFormat {
+  /**
+   * Get a singleton instance of a Tlv0_1WireFormat.
+   * @return The singleton instance.
+   */
+  public static Tlv0_1WireFormat
+  get()
+  {
+    return instance_;
+  }
+
+  private static Tlv0_1WireFormat instance_ = new Tlv0_1WireFormat();
+}
