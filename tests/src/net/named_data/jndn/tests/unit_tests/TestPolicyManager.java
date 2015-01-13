166690364af533f201044be82aa8287287066b2e
Jeff Thompson <jefft0@remap.ucla.edu>
2015-01-13 09:32:29 -0800

    unit_tests: Added initial TestPolicyManager.

---
+++ tests/src/net/named_data/jndn/tests/unit_tests/TestPolicyManager.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (C) 2015 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * From PyNDN unit-tests by Adeola Bannis.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+package net.named_data.jndn.tests.unit_tests;
+
+import org.junit.After;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ */
+public class TestPolicyManager {
+  @Before
+  public void
+  setUp()
+  {
+  }
+
+  @After
+  public void
+  tearDown()
+  {
+  }
+
+  @Test
+  public void
+  testNoVerify()
+  {
+    throw new Error("dir " + System.getProperty("user.dir"));
+  }
+}
