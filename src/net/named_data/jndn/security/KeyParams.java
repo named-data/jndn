f520b2708987e81e6ce2b92a718e1871e8cbb24d
Jeff Thompson <jefft0@remap.ucla.edu>
2015-01-29 15:43:20 -0800

    security: Added KeyParams, RsaKeyParams, EcdsaKeyParams.

---
+++ src/net/named_data/jndn/security/KeyParams.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (C) 2015 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * @author: From code in ndn-cxx by Yingdi Yu <yingdi@cs.ucla.edu>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+package net.named_data.jndn.security;
+
+/**
+ * KeyParams is a base class for key parameters. Its subclasses are used to
+ * store parameters for key generation.
+ */
+public class KeyParams {
+  public KeyType
+  getKeyType()
+  {
+    return keyType_;
+  }
+
+  protected KeyParams(KeyType keyType)
+  {
+    keyType_ = keyType;
+  }
+
+  private final KeyType keyType_;
+}
---
+++ src/net/named_data/jndn/security/RsaKeyParams.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (C) 2015 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * @author: From code in ndn-cxx by Yingdi Yu <yingdi@cs.ucla.edu>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+package net.named_data.jndn.security;
+
+public class RsaKeyParams extends KeyParams {
+  public RsaKeyParams(int size)
+  {
+    super(getType());
+    size_ = size;
+  }
+
+  public RsaKeyParams()
+  {
+    super(getType());
+    size_ = getDefaultSize();
+  }
+
+  public int
+  getKeySize()
+  {
+    return size_;
+  }
+
+  private static int
+  getDefaultSize() { return 2048; }
+
+  private static KeyType
+  getType() { return KeyType.RSA; }
+
+  private final int size_;
+}
