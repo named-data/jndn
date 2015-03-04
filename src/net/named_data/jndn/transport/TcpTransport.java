32238243bc784f0f431fe0ed7c3d6f1448896edc
Jeff Thompson <jefft0@remap.ucla.edu>
2015-03-04 11:27:39 -0800

    transport (minor): Update the doc comment for TcpTransport.isLocal.

--- src/net/named_data/jndn/transport/TcpTransport.java
+++ src/net/named_data/jndn/transport/TcpTransport.java
@@ -83,9 +83,8 @@ public class TcpTransport extends Transport {
    * contains a host name, InetAddress will do a blocking DNS lookup; otherwise
    * it will parse the IP address and examine the first octet to determine if
    * it is a loopback address (e.g. first octet == 127).
-   *
-   * @param connectionInfo
-   * @return 
+   * @param connectionInfo A TcpTransport.ConnectionInfo with the host to check.
+   * @return True if the host is local, false if not.
    * @throws java.io.IOException 
    */
   public boolean isLocal(Transport.ConnectionInfo connectionInfo) 
