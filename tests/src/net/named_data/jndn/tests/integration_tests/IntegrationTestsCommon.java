005e2de7ce69d51555f8d2031f13e70e68857667
Jeff Thompson <jefft0@remap.ucla.edu>
2015-07-13 08:27:53 -0700

    integration_tests (minor): Restore formatting that was changed by the pull request.

--- tests/src/net/named_data/jndn/tests/integration_tests/IntegrationTestsCommon.java
+++ tests/src/net/named_data/jndn/tests/integration_tests/IntegrationTestsCommon.java
@@ -1,22 +1,23 @@
 /**
  * Copyright (C) 2015 Regents of the University of California.
- *
  * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * @author: Andrew Brown <andrew.brown@intel.com>
  *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>. A copy
- * of the GNU Lesser General Public License is in the file COPYING.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
  */
+
 package src.net.named_data.jndn.tests.integration_tests;
 
 import java.io.File;
@@ -204,15 +205,17 @@ public class IntegrationTestsCommon {
   });
 }
 
-class VerifyCounter implements OnVerified, OnVerifyFailed {
-
+class VerifyCounter implements OnVerified, OnVerifyFailed
+{
   public void
-    onVerified(Data data) {
+  onVerified(Data data)
+  {
     ++onVerifiedCallCount_;
   }
 
   public void
-    onVerifyFailed(Data data) {
+  onVerifyFailed(Data data)
+  {
     ++onVerifyFailedCallCount_;
   }
 
