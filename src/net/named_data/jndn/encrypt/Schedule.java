2fcd21e36fe340597e2ab8122dfd0eaf24641632
Jeff Thompson <jefft0@remap.ucla.edu>
2015-10-21 08:30:17 -0700

    encrypt: In Schedule, added wireEncode/Decode.

--- src/net/named_data/jndn/encrypt/Schedule.java
+++ src/net/named_data/jndn/encrypt/Schedule.java
@@ -20,8 +20,18 @@
 
 package net.named_data.jndn.encrypt;
 
+import java.nio.ByteBuffer;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Iterator;
+import java.util.TimeZone;
 import java.util.TreeSet;
+import net.named_data.jndn.encoding.EncodingException;
+import net.named_data.jndn.encoding.tlv.Tlv;
+import net.named_data.jndn.encoding.tlv.TlvDecoder;
+import net.named_data.jndn.encoding.tlv.TlvEncoder;
+import net.named_data.jndn.util.Blob;
 
 /**
  * Schedule is used to manage the times when a member can access data using two
@@ -156,8 +166,185 @@ public class Schedule {
       return whiteNegativeResult;
     }
   }
-  
+
+  /**
+   * Encode this Schedule.
+   * @return The encoded buffer.
+   */
+  public final Blob
+  wireEncode()
+  {
+    // For now, don't use WireFormat and hardcode to use TLV since the encoding
+    // doesn't go out over the wire, only into the local SQL database.
+    TlvEncoder encoder = new TlvEncoder(256);
+    int saveLength = encoder.getLength();
+
+    // Encode backwards.
+    // Encode the blackIntervalList.
+    int saveLengthForList = encoder.getLength();
+    for (Iterator i = blackIntervalList_.descendingIterator(); i.hasNext(); ) {
+      RepetitiveInterval element = (RepetitiveInterval)i.next();
+      encodeRepetitiveInterval(element, encoder);
+    }
+    encoder.writeTypeAndLength
+      (Tlv.Encrypt_BlackIntervalList, encoder.getLength() - saveLengthForList);
+
+    // Encode the whiteIntervalList.
+    saveLengthForList = encoder.getLength();
+    for (Iterator i = whiteIntervalList_.descendingIterator(); i.hasNext(); ) {
+      RepetitiveInterval element = (RepetitiveInterval)i.next();
+      encodeRepetitiveInterval(element, encoder);
+    }
+    encoder.writeTypeAndLength
+      (Tlv.Encrypt_WhiteIntervalList, encoder.getLength() - saveLengthForList);
+
+    encoder.writeTypeAndLength
+      (Tlv.Encrypt_Schedule, encoder.getLength() - saveLength);
+
+    return new Blob(encoder.getOutput(), false);
+  }
+
+  /**
+   * Decode the input and update this Schedule object.
+   * @param input The input buffer to decode.  This reads from position() to
+   * limit(), but does not change the position.
+   * @throws EncodingException For invalid encoding.
+   */
+  public final void
+  wireDecode(ByteBuffer input) throws EncodingException
+  {
+    // For now, don't use WireFormat and hardcode to use TLV since the encoding
+    // doesn't go out over the wire, only into the local SQL database.
+    TlvDecoder decoder = new TlvDecoder(input);
+
+    int endOffset = decoder.readNestedTlvsStart(Tlv.Encrypt_Schedule);
+
+    // Decode the whiteIntervalList.
+    whiteIntervalList_.clear();
+    int listEndOffset = decoder.readNestedTlvsStart(Tlv.Encrypt_WhiteIntervalList);
+    while (decoder.getOffset() < listEndOffset)
+      whiteIntervalList_.add(decodeRepetitiveInterval(decoder));
+    decoder.finishNestedTlvs(listEndOffset);
+
+    // Decode the blackIntervalList.
+    blackIntervalList_.clear();
+    listEndOffset = decoder.readNestedTlvsStart(Tlv.Encrypt_BlackIntervalList);
+    while (decoder.getOffset() < listEndOffset)
+      blackIntervalList_.add(decodeRepetitiveInterval(decoder));
+    decoder.finishNestedTlvs(listEndOffset);
+    
+    decoder.finishNestedTlvs(endOffset);
+  }
+
+  /**
+   * Decode the input and update this Schedule object.
+   * @param input The input buffer to decode.  This reads from position() to
+   * limit(), but does not change the position.
+   * @throws EncodingException For invalid encoding.
+   */
+  public final void
+  wireDecode(Blob input) throws EncodingException
+  {
+    wireDecode(input.buf());
+  }
+
+  /**
+   * Encode the RepetitiveInterval as NDN-TLV to the encoder.
+   * @param repetitiveInterval The RepetitiveInterval to encode.
+   * @param encoder The TlvEncoder to receive the encoding.
+   */
+  private static void
+  encodeRepetitiveInterval
+    (RepetitiveInterval repetitiveInterval, TlvEncoder encoder)
+  {
+    int saveLength = encoder.getLength();
+
+    // Encode backwards.
+    encoder.writeNonNegativeIntegerTlv
+      (Tlv.Encrypt_RepeatUnit, repetitiveInterval.getRepeatUnit().getNumericType());
+    encoder.writeNonNegativeIntegerTlv
+      (Tlv.Encrypt_NRepeats, repetitiveInterval.getNRepeats());
+    encoder.writeNonNegativeIntegerTlv
+      (Tlv.Encrypt_IntervalEndHour, repetitiveInterval.getIntervalEndHour());
+    encoder.writeNonNegativeIntegerTlv
+      (Tlv.Encrypt_IntervalStartHour, repetitiveInterval.getIntervalStartHour());
+    // Use Blob to convert the string to UTF8 encoding.
+    encoder.writeBlobTlv(Tlv.Encrypt_EndDate,
+      new Blob(formatDate(repetitiveInterval.getEndDate())).buf());
+    encoder.writeBlobTlv(Tlv.Encrypt_StartDate,
+      new Blob(formatDate(repetitiveInterval.getStartDate())).buf());
+
+    encoder.writeTypeAndLength
+      (Tlv.Encrypt_RepetitiveInterval, encoder.getLength() - saveLength);
+  }
+
+  /**
+   * Decode the input as an NDN-TLV RepetitiveInterval.
+   * @param decoder The decoder with the input to decode.
+   * @return A new RepetitiveInterval with the decoded result.
+   */
+  private static RepetitiveInterval
+  decodeRepetitiveInterval(TlvDecoder decoder) throws EncodingException
+  {
+    int endOffset = decoder.readNestedTlvsStart(Tlv.Encrypt_RepetitiveInterval);
+
+    // Use Blob to convert UTF8 to a string.
+    double startDate = parseDate
+      (new Blob(decoder.readBlobTlv(Tlv.Encrypt_StartDate), true).toString());
+    double endDate = parseDate
+      (new Blob(decoder.readBlobTlv(Tlv.Encrypt_EndDate), true).toString());
+    int startHour = (int)decoder.readNonNegativeIntegerTlv
+      (Tlv.Encrypt_IntervalStartHour);
+    int endHour = (int)decoder.readNonNegativeIntegerTlv
+      (Tlv.Encrypt_IntervalEndHour);
+    int nRepeats = (int)decoder.readNonNegativeIntegerTlv(Tlv.Encrypt_NRepeats);
+    
+    int repeatUnitCode = (int)decoder.readNonNegativeIntegerTlv
+      (Tlv.Encrypt_RepeatUnit);
+    RepetitiveInterval.RepeatUnit repeatUnit;
+    if (repeatUnitCode == RepetitiveInterval.RepeatUnit.NONE.getNumericType())
+      repeatUnit = RepetitiveInterval.RepeatUnit.NONE;
+    else if (repeatUnitCode == RepetitiveInterval.RepeatUnit.DAY.getNumericType())
+      repeatUnit = RepetitiveInterval.RepeatUnit.DAY;
+    else if (repeatUnitCode == RepetitiveInterval.RepeatUnit.MONTH.getNumericType())
+      repeatUnit = RepetitiveInterval.RepeatUnit.MONTH;
+    else if (repeatUnitCode == RepetitiveInterval.RepeatUnit.YEAR.getNumericType())
+      repeatUnit = RepetitiveInterval.RepeatUnit.YEAR;
+    else
+      throw new EncodingException
+        ("Unrecognized RepetitiveInterval RepeatUnit code: " + repeatUnitCode);
+
+    decoder.finishNestedTlvs(endOffset);
+    return new RepetitiveInterval
+      (startDate, endDate, startHour, endHour, nRepeats, repeatUnit);
+  }
+
+  private static double
+  parseDate(String dateString) throws EncodingException
+  {
+    try {
+      return (double)dateFormat.parse(dateString).getTime();
+    } catch (ParseException ex) {
+      throw new EncodingException("Cannot parse date string " + dateString);
+    }
+  }
+
+  private static String
+  formatDate(double msSince1970)
+  {
+    return dateFormat.format(new Date((long)Math.round(msSince1970)));
+  }
+
+  private static SimpleDateFormat
+  getDateFormat()
+  {
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd'T'HHmmss");
+    dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+    return dateFormat;
+  }
+
   // Use TreeSet without generics so it works with older Java compilers.
   private final TreeSet whiteIntervalList_ = new TreeSet(); // of RepetitiveInterval
   private final TreeSet blackIntervalList_ = new TreeSet(); // of RepetitiveInterval
+  private static final SimpleDateFormat dateFormat = getDateFormat();
 }
