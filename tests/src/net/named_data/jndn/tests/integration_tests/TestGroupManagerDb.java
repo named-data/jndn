5bf9a09468e47651692f9c2f7f6b7e6849bbf653
Jeff Thompson <jefft0@remap.ucla.edu>
2015-10-28 16:21:34 -0700

    integration_tests (minor): Fix doc header for TestGroupManagerDb.

--- tests/src/net/named_data/jndn/tests/integration_tests/TestGroupManagerDb.java
+++ tests/src/net/named_data/jndn/tests/integration_tests/TestGroupManagerDb.java
@@ -1,7 +1,8 @@
 /**
  * Copyright (C) 2015 Regents of the University of California.
  * @author: Jeff Thompson <jefft0@remap.ucla.edu>
- * From PyNDN unit-tests by Adeola Bannis.
+ * @author: From ndn-group-encrypt unit tests
+ * https://github.com/named-data/ndn-group-encrypt/blob/master/tests/unit-tests/group-manager-db.t.cpp
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
