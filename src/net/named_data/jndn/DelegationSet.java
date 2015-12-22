e1f29364fd2e007f5435813c192ee1a013feb6db
Jeff Thompson <jefft0@remap.ucla.edu>
2015-12-22 13:10:58 -0800

    DelegationSet (internal): Use public Delegation.getName().

--- src/net/named_data/jndn/DelegationSet.java
+++ src/net/named_data/jndn/DelegationSet.java
@@ -160,7 +160,7 @@ public class DelegationSet {
     boolean wasRemoved = false;
     // Go backwards through the list so we can remove entries.
     for (int i = delegations_.size() - 1; i >= 0; --i) {
-      if (((Delegation)delegations_.get(i)).name_.equals(name)) {
+      if (((Delegation)delegations_.get(i)).getName().equals(name)) {
         wasRemoved = true;
         delegations_.remove(i);
       }
@@ -203,7 +203,7 @@ public class DelegationSet {
   find(Name name)
   {
     for (int i = 0; i < delegations_.size(); ++i) {
-      if (((Delegation)delegations_.get(i)).name_.equals(name))
+      if (((Delegation)delegations_.get(i)).getName().equals(name))
         return i;
     }
 
