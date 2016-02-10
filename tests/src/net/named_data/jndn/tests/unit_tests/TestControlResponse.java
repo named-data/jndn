ad0ae04e7087216843f9e24a0f89cefbca4b37e3
Jeff Thompson <jefft0@remap.ucla.edu>
2016-02-10 15:33:47 -0800

    unit_tests: In TestControlResponse, use the original copyright header.

--- tests/src/net/named_data/jndn/tests/unit_tests/TestControlResponse.java
+++ tests/src/net/named_data/jndn/tests/unit_tests/TestControlResponse.java
@@ -1,20 +1,15 @@
-/**
- * Copyright (C) 2015-2016 Regents of the University of California.
- * @author: Andrew Brown <andrew.brown@intel.com>
+/*
+ * jndn-management
+ * Copyright (c) 2015, Intel Corporation.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 3, as published by the Free Software Foundation.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * A copy of the GNU Lesser General Public License is in the file COPYING.
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
  */
 
 package net.named_data.jndn.tests.unit_tests;
