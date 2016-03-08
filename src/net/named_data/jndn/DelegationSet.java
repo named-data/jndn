791b0bb90b7c2b32a6898fbdbc5121a9bf649ebf
Jeff Thompson <jefft0@remap.ucla.edu>
2016-03-08 14:25:59 -0800

    DelegationSet (minor): Fix typo on doc comment.

--- src/net/named_data/jndn/DelegationSet.java
+++ src/net/named_data/jndn/DelegationSet.java
@@ -151,7 +151,7 @@ public class DelegationSet {
 
   /**
    * Remove every DelegationSet.Delegation with the given name.
-   * @param name Then name to match the name of the delegation(s) to be removed.
+   * @param name The name to match the name of the delegation(s) to be removed.
    * @return True if a DelegationSet.Delegation was removed, otherwise false.
    */
   public final boolean
