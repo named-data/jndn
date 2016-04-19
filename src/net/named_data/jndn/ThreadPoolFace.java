72e9b76a768df59a6247899bdedfa326b85d6d69
Jeff Thompson <jefft0@remap.ucla.edu>
2016-04-19 10:46:41 -0700

    ThreadPoolFace: Added overrides for registerPrefix and setInterestFilter to wrap callbacks to submit to the thread pool. Refs #2565.

--- src/net/named_data/jndn/ThreadPoolFace.java
+++ src/net/named_data/jndn/ThreadPoolFace.java
@@ -78,7 +78,7 @@ public class ThreadPoolFace extends Face {
   {
     final long pendingInterestId = node_.getNextEntryId();
 
-    // Wrap onData and onTimeout to submit to the thread pool.
+    // Wrap callbacks to submit to the thread pool.
     final OnData finalOnData = onData;
     final OnData onDataSubmit = new OnData() {
       public void onData(final Interest localInterest, final Data data) {
@@ -97,22 +97,21 @@ public class ThreadPoolFace extends Face {
     };
 
     final OnTimeout finalOnTimeout = onTimeout;
-    final OnTimeout onTimeoutSubmit = onTimeout == null ? null :
-      new OnTimeout() {
-        public void onTimeout(final Interest localInterest) {
-          threadPool_.submit(new Runnable() {
-            // Call the passed-in onTimeout.
-            public void run() {
-              // Need to catch and log exceptions at this async entry point.
-              try {
-                finalOnTimeout.onTimeout(localInterest);
-              } catch (Throwable ex) {
-                logger_.log(Level.SEVERE, "Error in onTimeout", ex);
-              }
+    final OnTimeout onTimeoutSubmit = onTimeout == null ? null : new OnTimeout() {
+      public void onTimeout(final Interest localInterest) {
+        threadPool_.submit(new Runnable() {
+          // Call the passed-in onTimeout.
+          public void run() {
+            // Need to catch and log exceptions at this async entry point.
+            try {
+              finalOnTimeout.onTimeout(localInterest);
+            } catch (Throwable ex) {
+              logger_.log(Level.SEVERE, "Error in onTimeout", ex);
             }
-          });
-        }
-      };
+          }
+        });
+      }
+    };
 
     threadPool_.submit(new Runnable() {
       public void run() {
@@ -128,6 +127,184 @@ public class ThreadPoolFace extends Face {
     });
 
     return pendingInterestId;
+  }
+
+  /**
+   * Submit a task to the thread pool to register prefix with the connected
+   * forwarder and call onInterest when a matching interest is received. To
+   * register a prefix with NFD, you must first call setCommandSigningInfo.
+   * @param prefix A Name for the prefix to register. This copies the Name.
+   * @param onInterest If not null, this creates an interest filter from prefix
+   * so that when an Interest is received which matches the filter, this calls
+   * onInterest.onInterest(prefix, interest, face, interestFilterId, filter).
+   * The onInterest callback should supply the Data with face.putData().
+   * NOTE: You must not change the prefix or filter objects - if you need to
+   * change them then make a copy.
+   * If onInterest is null, it is ignored and you must call setInterestFilter.
+   * NOTE: The library will log any exceptions thrown by this callback, but for
+   * better error handling the callback should catch and properly handle any
+   * exceptions.
+   * @param onRegisterFailed If register prefix fails for any reason, this
+   * calls onRegisterFailed.onRegisterFailed(prefix).
+   * NOTE: The library will log any exceptions thrown by this callback, but for
+   * better error handling the callback should catch and properly handle any
+   * exceptions.
+   * @param onRegisterSuccess This calls
+   * onRegisterSuccess.onRegisterSuccess(prefix, registeredPrefixId) when this
+   * receives a success message from the forwarder. If onRegisterSuccess is null,
+   * this does not use it. (The onRegisterSuccess parameter comes after
+   * onRegisterFailed because it can be null or omitted, unlike onRegisterFailed.)
+   * NOTE: The library will log any exceptions thrown by this callback, but for
+   * better error handling the callback should catch and properly handle any
+   * exceptions.
+   * @param flags The flags for finer control of which interests are forwarded
+   * to the application.
+   * @param wireFormat A WireFormat object used to encode the message.
+   * @return The registered prefix ID which can be used with
+   * removeRegisteredPrefix.
+   */
+  public long
+  registerPrefix
+    (final Name prefix, OnInterestCallback onInterest,
+     OnRegisterFailed onRegisterFailed, OnRegisterSuccess onRegisterSuccess,
+     final ForwardingFlags flags, final WireFormat wireFormat)
+    throws IOException, SecurityException
+  {
+    final long registeredPrefixId = node_.getNextEntryId();
+
+    // Wrap callbacks to submit to the thread pool.
+    final OnInterestCallback finalOnInterest = onInterest;
+    final OnInterestCallback onInterestSubmit =
+        onInterest == null ? null : new OnInterestCallback() {
+      public void onInterest(final Name localPrefix, final Interest interest,
+          final Face face, final long interestFilterId, final InterestFilter filter) {
+        threadPool_.submit(new Runnable() {
+          // Call the passed-in onInterest.
+          public void run() {
+            // Need to catch and log exceptions at this async entry point.
+            try {
+              finalOnInterest.onInterest
+                (localPrefix, interest, face, interestFilterId, filter);
+            } catch (Throwable ex) {
+              logger_.log(Level.SEVERE, "Error in onInterest", ex);
+            }
+          }
+        });
+      }
+    };
+
+    final OnRegisterFailed finalOnRegisterFailed = onRegisterFailed;
+    final OnRegisterFailed onRegisterFailedSubmit =
+        new OnRegisterFailed() {
+      public void onRegisterFailed(final Name localPrefix) {
+        threadPool_.submit(new Runnable() {
+          // Call the passed-in onRegisterFailed.
+          public void run() {
+            // Need to catch and log exceptions at this async entry point.
+            try {
+              finalOnRegisterFailed.onRegisterFailed(localPrefix);
+            } catch (Throwable ex) {
+              logger_.log(Level.SEVERE, "Error in onRegisterFailed", ex);
+            }
+          }
+        });
+      }
+    };
+
+    // Wrap callbacks to submit to the thread pool.
+    final OnRegisterSuccess finalOnRegisterSuccess = onRegisterSuccess;
+    final OnRegisterSuccess onRegisterSuccessSubmit =
+        onRegisterSuccess == null ? null : new OnRegisterSuccess() {
+      public void onRegisterSuccess(final Name localPrefix,
+                                    final long localRegisteredPrefixId) {
+        threadPool_.submit(new Runnable() {
+          // Call the passed-in onRegisterSuccess.
+          public void run() {
+            // Need to catch and log exceptions at this async entry point.
+            try {
+              finalOnRegisterSuccess.onRegisterSuccess
+                (localPrefix, localRegisteredPrefixId);
+            } catch (Throwable ex) {
+              logger_.log(Level.SEVERE, "Error in onRegisterSuccess", ex);
+            }
+          }
+        });
+      }
+    };
+
+    threadPool_.submit(new Runnable() {
+      public void run() {
+        // Need to catch and log exceptions at this async entry point.
+        try {
+          node_.registerPrefix
+            (registeredPrefixId, prefix, onInterestSubmit, onRegisterFailedSubmit,
+             onRegisterSuccessSubmit, flags, wireFormat, 
+             commandKeyChain_, commandCertificateName_, ThreadPoolFace.this);
+        } catch (Throwable ex) {
+          logger_.log(Level.SEVERE, null, ex);
+        }
+      }
+    });
+
+    return registeredPrefixId;
+  }
+
+  /**
+   * Submit a task to the thread pool to add an entry to the local interest
+   * filter table to call the onInterest callback for a matching incoming
+   * Interest. This method only modifies the library's local callback table and
+   * does not register the prefix with the forwarder. It will always succeed.
+   * To register a prefix with the forwarder, use registerPrefix.
+   * @param filter The InterestFilter with a prefix and optional regex filter
+   * used to match the name of an incoming Interest. This makes a copy of filter.
+   * @param onInterest When an Interest is received which matches the filter,
+   * this calls
+   * onInterest.onInterest(prefix, interest, face, interestFilterId, filter).
+   * NOTE: The library will log any exceptions thrown by this callback, but for
+   * better error handling the callback should catch and properly handle any
+   * exceptions.
+   * @return The interest filter ID which can be used with unsetInterestFilter.
+   */
+  public long
+  setInterestFilter
+    (final InterestFilter filter, final OnInterestCallback onInterest)
+  {
+    final long interestFilterId = node_.getNextEntryId();
+
+        // Wrap callbacks to submit to the thread pool.
+    final OnInterestCallback finalOnInterest = onInterest;
+    final OnInterestCallback onInterestSubmit = new OnInterestCallback() {
+      public void onInterest(final Name prefix, final Interest interest,
+          final Face face, final long interestFilterId, final InterestFilter filter) {
+        threadPool_.submit(new Runnable() {
+          // Call the passed-in onInterest.
+          public void run() {
+            // Need to catch and log exceptions at this async entry point.
+            try {
+              finalOnInterest.onInterest
+                (prefix, interest, face, interestFilterId, filter);
+            } catch (Throwable ex) {
+              logger_.log(Level.SEVERE, "Error in onInterest", ex);
+            }
+          }
+        });
+      }
+    };
+
+    threadPool_.submit(new Runnable() {
+      public void run() {
+        // Need to catch and log exceptions at this async entry point.
+        try {
+          node_.setInterestFilter
+            (interestFilterId, filter, onInterestSubmit, ThreadPoolFace.this);
+        } catch (Throwable ex) {
+          logger_.log(Level.SEVERE, null, ex);
+        }
+      }
+    });
+
+
+    return interestFilterId;
   }
 
   /**
