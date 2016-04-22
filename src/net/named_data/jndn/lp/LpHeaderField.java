b6834e3bafd7fde33294922183698b0567965d04
Jeff Thompson <jefft0@remap.ucla.edu>
2016-04-22 16:27:07 -0700

    lp: Added LpPacket.

---
+++ src/net/named_data/jndn/lp/LpHeaderField.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (C) 2016 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * @author: From ndn-cxx field.hpp https://github.com/named-data/ndn-cxx/blob/master/src/lp/field.hpp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+package net.named_data.jndn.lp;
+
+/**
+ * LpHeaderField is an abstract base class for NDNLPv2 header fields such as
+ * NetworkNack.
+ * http://redmine.named-data.net/projects/nfd/wiki/NDNLPv2
+ */
+public abstract class LpHeaderField {
+}
