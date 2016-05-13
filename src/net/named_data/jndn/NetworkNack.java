f5d5b1de972d014333bfe7c8d445f4c02084afde
Jeff Thompson <jefft0@remap.ucla.edu>
2016-05-13 11:14:54 -0700

    NetworkNack (internal): Change the Reason OTHER_CODE enum to 0x7fff.

--- src/net/named_data/jndn/NetworkNack.java
+++ src/net/named_data/jndn/NetworkNack.java
@@ -34,10 +34,10 @@ public class NetworkNack {
    */
   public static enum Reason {
     NONE      (0),
-    OTHER_CODE(1),
     CONGESTION(50),
     DUPLICATE (100),
-    NO_ROUTE  (150);
+    NO_ROUTE  (150),
+    OTHER_CODE(0x7fff);
 
     Reason (int type)
     {
