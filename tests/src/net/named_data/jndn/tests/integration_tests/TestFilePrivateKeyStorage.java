923c1a10b47faf2eefece4fffe5233dcc40f37da
Jeff Thompson <jefft0@remap.ucla.edu>
2017-02-21 12:52:40 -0800

    integration_tests (internal): In TestFilePrivateKeyStorage, use Common.getHomeDirectory().

--- tests/src/net/named_data/jndn/tests/integration_tests/TestFilePrivateKeyStorage.java
+++ tests/src/net/named_data/jndn/tests/integration_tests/TestFilePrivateKeyStorage.java
@@ -23,11 +23,11 @@ import java.nio.ByteBuffer;
 import net.named_data.jndn.Name;
 import net.named_data.jndn.security.DigestAlgorithm;
 import net.named_data.jndn.security.KeyClass;
-import net.named_data.jndn.security.KeyType;
 import net.named_data.jndn.security.RsaKeyParams;
 import net.named_data.jndn.security.certificate.PublicKey;
 import net.named_data.jndn.security.identity.FilePrivateKeyStorage;
 import net.named_data.jndn.util.Blob;
+import net.named_data.jndn.util.Common;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -38,7 +38,7 @@ public class TestFilePrivateKeyStorage {
   /**
    * Keep a reference to the key storage folder
    */
-  private static final File ndnFolder_ = new File (new File(System.getProperty("user.home", "."), ".ndn"), "ndnsec-tpm-file");;
+  private static final File ndnFolder_ = new File (new File(Common.getHomeDirectory(), ".ndn"), "ndnsec-tpm-file");;
 
   /**
    * Create a few keys before testing
