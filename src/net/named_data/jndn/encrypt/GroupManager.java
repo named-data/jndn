b3621a1d1c1b5a5fed2554aa5db120e7eae751f7
Jeff Thompson <jefft0@remap.ucla.edu>
2017-05-30 16:02:46 -0700

    encrypt: In GroupManager and Producer, add throws for security v2 sign. Refs #4045.

--- src/net/named_data/jndn/encrypt/GroupManager.java
+++ src/net/named_data/jndn/encrypt/GroupManager.java
@@ -37,6 +37,8 @@ import net.named_data.jndn.security.KeyChain;
 import net.named_data.jndn.security.RsaKeyParams;
 import net.named_data.jndn.security.SecurityException;
 import net.named_data.jndn.security.certificate.IdentityCertificate;
+import net.named_data.jndn.security.pib.PibImpl;
+import net.named_data.jndn.security.tpm.TpmBackEnd;
 import net.named_data.jndn.util.Blob;
 
 /**
@@ -89,7 +91,8 @@ public class GroupManager {
    */
   public final List
   getGroupKey(double timeSlot, boolean needRegenerate)
-    throws GroupManagerDb.Error, SecurityException
+    throws GroupManagerDb.Error, SecurityException, TpmBackEnd.Error,
+      PibImpl.Error, KeyChain.Error
   {
     Map memberKeys = new TreeMap();
     List result = new ArrayList();
@@ -145,7 +148,9 @@ public class GroupManager {
    * Call the main getGroupKey where needRegenerate is default true.
    */
   public final List
-  getGroupKey(double timeSlot) throws GroupManagerDb.Error, SecurityException
+  getGroupKey(double timeSlot) 
+    throws GroupManagerDb.Error, SecurityException, TpmBackEnd.Error,
+      PibImpl.Error, KeyChain.Error
   {
     return getGroupKey(timeSlot, true);
   }
@@ -347,7 +352,7 @@ public class GroupManager {
    */
   private Data
   createEKeyData(String startTimeStamp, String endTimeStamp, Blob publicKeyBlob)
-    throws SecurityException
+    throws SecurityException, TpmBackEnd.Error, PibImpl.Error, KeyChain.Error
   {
     Name name = new Name(namespace_);
     name.append(Encryptor.NAME_COMPONENT_E_KEY).append(startTimeStamp)
@@ -377,7 +382,7 @@ public class GroupManager {
   createDKeyData
     (String startTimeStamp, String endTimeStamp, Name keyName,
      Blob privateKeyBlob, Blob certificateKey)
-    throws SecurityException
+    throws SecurityException, TpmBackEnd.Error, PibImpl.Error, KeyChain.Error
   {
     Name name = new Name(namespace_);
     name.append(Encryptor.NAME_COMPONENT_D_KEY);
@@ -478,13 +483,13 @@ public class GroupManager {
     createDKeyData
       (GroupManager groupManager, String startTimeStamp, String endTimeStamp,
        Name keyName, Blob privateKeyBlob, Blob certificateKey)
-      throws SecurityException;
+      throws SecurityException, TpmBackEnd.Error, PibImpl.Error, KeyChain.Error;
 
     public abstract Data
       createEKeyData
         (GroupManager groupManager, String startTimeStamp, String endTimeStamp,
          Blob publicKeyBlob)
-        throws SecurityException;
+        throws SecurityException, TpmBackEnd.Error, PibImpl.Error, KeyChain.Error;
   }
 
   /**
@@ -504,7 +509,7 @@ public class GroupManager {
     createDKeyData
       (GroupManager groupManager, String startTimeStamp, String endTimeStamp,
        Name keyName, Blob privateKeyBlob, Blob certificateKey)
-      throws SecurityException
+      throws SecurityException, TpmBackEnd.Error, PibImpl.Error, KeyChain.Error
     {
       return groupManager.createDKeyData
         (startTimeStamp, endTimeStamp, keyName, privateKeyBlob, certificateKey);
@@ -514,7 +519,7 @@ public class GroupManager {
       createEKeyData
         (GroupManager groupManager, String startTimeStamp, String endTimeStamp,
          Blob publicKeyBlob)
-        throws SecurityException
+        throws SecurityException, TpmBackEnd.Error, PibImpl.Error, KeyChain.Error
     {
       return groupManager.createEKeyData
         (startTimeStamp, endTimeStamp, publicKeyBlob);
