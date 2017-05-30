3a58d6d3b49d63dc5cda673663df5df0f9eb8a88
Jeff Thompson <jefft0@remap.ucla.edu>
2017-05-30 15:15:10 -0700

    security: Added Pib remaining methods. Refs #4045.

--- src/net/named_data/jndn/security/pib/Pib.java
+++ src/net/named_data/jndn/security/pib/Pib.java
@@ -20,6 +20,8 @@
 
 package net.named_data.jndn.security.pib;
 
+import net.named_data.jndn.Name;
+
 /**
  * In general, a PIB (Public Information Base) stores the public portion of a
  * user's cryptography keys. The format and location of stored information is
@@ -52,5 +54,170 @@ public class Pib {
     }
   }
 
+  /**
+   * Get the scheme of the PIB locator.
+   * @return The scheme string.
+   */
+  public final String
+  getScheme() { return scheme_; }
 
+  /**
+   * Get the PIB locator.
+   * @return The PIB locator.
+   */
+  public final String
+  getPibLocator() {return scheme_ + ":" + location_; }
+
+  /**
+   * Set the corresponding TPM information to tpmLocator.
+   * If the tpmLocator is different from the existing one, the PIB will be reset.
+   * Otherwise, nothing will be changed.
+   */
+  public final void
+  setTpmLocator(String tpmLocator) throws PibImpl.Error
+  {
+    if (tpmLocator.equals(pibImpl_.getTpmLocator()))
+      return;
+
+    reset_();
+    pibImpl_.setTpmLocator(tpmLocator);
+  }
+
+  /**
+   * Get the TPM Locator.
+   * @throws Pib.Error if the TPM locator is empty.
+   */
+  public final String
+  getTpmLocator() throws Pib.Error
+  {
+    String tpmLocator = pibImpl_.getTpmLocator();
+    if (tpmLocator.equals(""))
+      throw new Error("TPM info does not exist");
+
+    return tpmLocator;
+  }
+
+  /**
+   * Get the identity with name identityName.
+   * @param identityName The name of the identity.
+   * @return The PibIdentity object.
+   * @throws Pib::Error if the identity does not exist.
+   */
+  public final PibIdentity
+  getIdentity(Name identityName) throws PibImpl.Error, Pib.Error
+  {
+    // BOOST_ASSERT(identities_.isConsistent());
+
+    return identities_.get(identityName);
+  }
+
+  /**
+   * Get the default identity.
+   * @return The PibIdentity object.
+   * @throws Pib::Error if there is no default identity.
+   */
+  public final PibIdentity
+  getDefaultIdentity() throws PibImpl.Error, Error
+  {
+    // BOOST_ASSERT(identities_.isConsistent());
+
+    if (defaultIdentity_ == null)
+      defaultIdentity_ = identities_.get(pibImpl_.getDefaultIdentity());
+
+    // BOOST_ASSERT(pibImpl_->getDefaultIdentity() == defaultIdentity_->getName());
+
+    return defaultIdentity_;
+  }
+
+  /*
+   * Create a Pib instance. This constructor should only be called by KeyChain.
+   * @param scheme The scheme for the PIB.
+   * @param location The location for the PIB.
+   * @param pibImpl The PIB backend implementation.
+   */
+  public Pib(String scheme, String location, PibImpl pibImpl)
+    throws PibImpl.Error
+  {
+    scheme_ = scheme;
+    location_ = location;
+    identities_ = new PibIdentityContainer(pibImpl);
+    pibImpl_ = pibImpl;
+
+    if (pibImpl == null)
+      throw new AssertionError("The pibImpl is null");
+  }
+
+  /**
+   * Reset the content in the PIB, including a reset of the TPM locator.
+   * This should only be called by KeyChain.
+   */
+  public final void
+  reset_() throws PibImpl.Error
+  {
+    pibImpl_.clearIdentities();
+    pibImpl_.setTpmLocator("");
+    defaultIdentity_ = null;
+    identities_.reset();
+  }
+
+  /**
+   * Add an identity with name identityName. Create the identity if it does not
+   * exist.
+   * This should only be called by KeyChain.
+   * @param identityName The name of the identity, which is copied.
+   * @return The PibIdentity object.
+   */
+  public final PibIdentity
+  addIdentity_(Name identityName) throws PibImpl.Error, Pib.Error
+  {
+    // BOOST_ASSERT(identities_.isConsistent());
+
+    return identities_.add(identityName);
+  }
+
+  /**
+   * Remove the identity with name identityName, and its related keys and
+   * certificates. If the default identity is being removed, no default identity
+   * will be selected.  If the identity does not exist, do nothing.
+   * This should only be called by KeyChain.
+   * @param identityName The name of the identity.
+   */
+  public final void
+  removeIdentity_(Name identityName) throws PibImpl.Error
+  {
+    // BOOST_ASSERT(identities_.isConsistent());
+
+    if (defaultIdentity_ != null &&
+      defaultIdentity_.getName().equals(identityName))
+      defaultIdentity_ = null;
+
+    identities_.remove(identityName);
+  }
+
+  /**
+   * Set the identity with name identityName as the default identity.
+   * Create the identity if it does not exist.
+   * This should only be called by KeyChain.
+   * @param identityName The name of the identity.
+   * @return The PibIdentity object of the default identity.
+   */
+  public final PibIdentity
+  setDefaultIdentity_(Name identityName) throws PibImpl.Error, Error
+  {
+    // BOOST_ASSERT(identities_.isConsistent());
+
+    defaultIdentity_ = identities_.add(identityName);
+
+    pibImpl_.setDefaultIdentity(identityName);
+    return defaultIdentity_;
+  }
+
+  private final String scheme_;
+  private final String location_;
+
+  private PibIdentity defaultIdentity_ = null;
+
+  private final PibIdentityContainer identities_;
+
+  private final PibImpl pibImpl_;
 }
