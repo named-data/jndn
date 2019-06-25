e0fdc4053cc8a7f17a10f9d982e17ba2ee7e0195
Jeff Thompson <jefft0@remap.ucla.edu>
2019-06-25 13:16:14 -0700

    security (minor): In the ValidityPeriod getFromSignature doc comment, mention canGetFromSignature.

--- src/net/named_data/jndn/security/ValidityPeriod.java
+++ src/net/named_data/jndn/security/ValidityPeriod.java
@@ -182,7 +182,8 @@ public class ValidityPeriod implements ChangeCountable {
 
   /**
    * If the signature is a type that has a ValidityPeriod, then return it.
-   * Otherwise throw an error.
+   * Otherwise throw an error. To check if the signature has a ValidityPeriod
+   * without throwing an error, you can use canGetFromSignature().
    * @param signature An object of a subclass of Signature.
    * @return The signature's ValidityPeriod. It is an error if signature doesn't
    * have a ValidityPeriod.
